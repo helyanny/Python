a79ee8139f1c2350f9ef09199f772ee545dc246d
helyanny <helyanny@gmail.com>
2025-05-12 14:11:49 -0400

    Delete bandnamegenerator.py

--- bandnamegenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band name generator.")
-x = input("What is the name of the city you grew up in ?: \n")
-y = input("What is the name of your pet?: \n")
-print("Your band name could be", x, y)
