06c8f2c9ae6603431ea90adf5a16ad520671760b
helyanny <helyanny@gmail.com>
2025-05-12 14:07:50 -0400

    Delete hello.py

--- hello.py
+++
@@ -1,6 +0,0 @@
-# Author: Helyanny Perozo
-# Date: September 2024
-# This is a program to say hello to everyone
-print('Hello World!')
-print('Hello\neveryone')
-print("my favourite number is", 4, "!")
